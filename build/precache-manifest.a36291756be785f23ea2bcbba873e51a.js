1bcfebe10e8ef9f39319bef2819923720c9a90e8
Amy-bcx <69481610+amy-bcx@users.noreply.github.com>
2021-03-11 09:26:39 -0600

    Add files via upload

---
+++ build/precache-manifest.a36291756be785f23ea2bcbba873e51a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3964e54cd20c09d62ff333bb7c443c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c3726474b1475cd1d3",
+    "url": "/static/css/main.60f8f318.chunk.css"
+  },
+  {
+    "revision": "d80c4650c37f9bdba797",
+    "url": "/static/js/2.67ac5769.chunk.js"
+  },
+  {
+    "revision": "713ed5b43acca529bd626f99ce12be30",
+    "url": "/static/js/2.67ac5769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c3726474b1475cd1d3",
+    "url": "/static/js/main.83be1392.chunk.js"
+  },
+  {
+    "revision": "69231b181d67cafa7903",
+    "url": "/static/js/runtime-main.355f681a.js"
+  },
+  {
+    "revision": "0f5680dbe00545db89646d64c5a9c96f",
+    "url": "/static/media/Playlist.0f5680db.mp3"
+  },
+  {
+    "revision": "c9aa934cce6332685a3628f5da8bfc4a",
+    "url": "/static/media/video.c9aa934c.mp4"
+  }
+]);
